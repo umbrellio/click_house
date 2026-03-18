f1321a32baea420a3606c3cd97667c5c56f59a9f
Yuri Smirnov <tycoooon@gmail.com>
2026-03-18 17:56:58 +0300

    Limit retries (#6)

--- lib/click_house/middleware/raise_error.rb
+++ lib/click_house/middleware/raise_error.rb
@@ -4,17 +4,27 @@ module ClickHouse
   module Middleware
     class RaiseError < Faraday::Middleware
       EXCEPTION_CODE_HEADER = 'x-clickhouse-exception-code'
+      MAX_RETRIES = 5
+      SLEEP_INTERVAL = 0.5
 
       Faraday::Response.register_middleware self => self
 
       # @param env [Faraday::Env]
       def call(env)
-        super
-      rescue Faraday::ConnectionFailed => e
-        if e.message.include?('end of file reached')
-          retry
-        else
-          raise NetworkException, e.message, e.backtrace
+        retries = 0
+
+        begin
+          super
+        rescue Faraday::ConnectionFailed => e
+          if e.message.include?('end of file reached')
+            retries += 1
+            raise if retries > MAX_RETRIES
+
+            Kernel.sleep(SLEEP_INTERVAL)
+            retry
+          else
+            raise NetworkException, e.message, e.backtrace
+          end
         end
       end
 
